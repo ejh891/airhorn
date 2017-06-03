8ba8b30bea4b2a854937df9f56fa80b1808e7b9e
EJ Hammond <ejh891@gmail.com>
2017-06-02 21:28:45 -0400

    Disable button while audio is playing

--- src/Components/BababaButton/component.js
+++ src/Components/BababaButton/component.js
@@ -12,13 +12,22 @@ class BababaButton extends Component {
         this.setState({playing: true})
         this.props.incrementCounter();
     };
+
+    onAudioStart = () => {
+        document.getElementById("button").disabled = true;
+    }
+
+    onAudioEnd = () => {
+        document.getElementById("button").disabled = false;
+    }
+
     render() {
         return(
             <div>
                 <div id="action">
                     <input id="button" className={"btn btn-success " + styles.button} type="button" value="Bababa!" onClick={this.handleClick}/>
                 </div>
-                <ReactHowler src="/airhorn.mp3" playing={this.state.playing} />
+                <ReactHowler src="/airhorn.mp3" playing={this.state.playing} onPlay={this.onAudioStart} onEnd={this.onAudioEnd}/>
             </div>
         )
     }
