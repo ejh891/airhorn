cb0c1c05926aa50d1f57c904d73e8188b79d1eef
EJ Hammond <ehammond@vistaprint.com>
2017-06-07 11:15:44 -0400

    Convert presentational components to stateless components

--- src/Components/BababaButton/component.js
+++ src/Components/BababaButton/component.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import styles from './styles.js';
 
-class BababaButton extends Component {
-    onClick = () => {
-        this.props.audioPlay();
-        this.props.incrementCounter();
-    }
-    render() {
-        return(
-            <div>
-                <div id="action">
-                    <input 
-                        id="button" 
-                        className="btn btn-success" 
-                        style={styles.button} 
-                        type="button" 
-                        value="Bababa!" 
-                        disabled={this.props.buttonDisabled}
-                        onClick={this.onClick}
-                    />
-                </div>
-
+const BababaButton = (props) => {
+    return(
+        <div>
+            <div id="action">
+                <input 
+                    id="button" 
+                    className="btn btn-success" 
+                    style={styles.button} 
+                    type="button" 
+                    value="Bababa!" 
+                    disabled={props.buttonDisabled}
+                    onClick={() => {props.audioPlay(); props.incrementCounter();}}
+                />
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default BababaButton;
--- src/Components/Counter/component.js
+++ src/Components/Counter/component.js
@@ -1,16 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './styles.js';
 
-class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
-        return(
-            <div id="count" style={styles.count}>{this.props.count}</div>
-        )
-    }
+const Counter = (props) => {
+    return(
+        <div id="count" style={styles.count}>{props.count}</div>
+    )
 }
 
 export default Counter;
